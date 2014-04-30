db7b5a932d9a54bf24977b5a3e38bfc70d1e1c76
yuanbo07 <y_wang@etud.insa-toulouse.fr>
2014-04-30 21:34:02 +0200

    classe label updated

--- Label.java
+++ Label.java
@@ -1,5 +1,27 @@
 package core;
 
 public class Label {
+	
+	
+	/* Pour mettre en oeuvre l'algorithme de Dijkstra, on associe à chaque sommet un label composé d'un quadruplet 
+	 * (marquage, cout, pere, sommet courant)
+	 * 
+	 * marquage : un booléen (valant vrai si le sommet est définitivement fixé par l'algorithme)
+	 * cout : la valeur courante du plus court chemin depuis l'origine vers le sommet
+	 * pere : correspond au sommet précédent sur le chemin correspondant au plus court chemin courant
+	 * Le sommet courant est le sommet associé à ce label (sommet ou numéro de sommet)
+	 */
+	
+	private Noeud sommetCourant;
+	private Noeud sommetPere;
+	private double coutCourant;
+	private boolean marquage;
+	
+	public Label(Noeud sommetCourant, Noeud sommetPere, double coutCourant){
+		this.sommetCourant = sommetCourant;
+		this.sommetPere = sommetPere;
+		this.coutCourant = coutCourant;
+		this.marquage = false;
+	}
 
 }
