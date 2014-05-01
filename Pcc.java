c91fa46882f324f968de2144d5df3d93e87868ee
yuanbo07 <y_wang@etud.insa-toulouse.fr>
2014-05-02 00:06:02 +0200

    edited Dijkstra

--- Pcc.java
+++ Pcc.java
@@ -14,6 +14,12 @@ public class Pcc extends Algo {
     protected int destination ;
     
 	private int numNoeudGraphe = this.graphe.getListeNoeuds().size();
+	
+	// on a le tas pour y mettre tous les labels (pour ordonner le coutCourant)
+	BinaryHeap<Label> tasLabel= new BinaryHeap<Label>();
+	
+	// une liste qui stocke les labels de tous les noeuds
+	ArrayList<Label> listeLabel = new ArrayList<Label>();
 
 	public Pcc(Graphe gr, PrintStream sortie, Readarg readarg) {
 		super(gr, sortie, readarg) ;
@@ -27,19 +33,6 @@ public class Pcc extends Algo {
 		this.zoneDestination = gr.getZone () ;
 		this.destination = readarg.lireInt ("Numero du sommet destination ? ");
     }
-	
-	/*
-	 * Principe de Dijkstra
-	 * 
-	 * 1. phase d'initialisation
-	 * on initialise noeud départ
-	 * on initialise cout de tous les sommets : infini -> marque = false
-	 * 
-	 * 2. on cherche successeurs de sommet origin
-	 * 
-	 * 3. on met à jour la distance
-	 * 
-	 */
 	
 	// fonctions qui vérifient la bonne saisie de numéro de noeuds
 	
@@ -62,52 +55,53 @@ public class Pcc extends Algo {
 		}
 		return trouveDest;
 	}
-	
-	
-    // phase d'initialisation : on met tous les noeuds "non marqué" 
+
+	// initialisation de tous les labels
 	
     public void initialisation(){
     	
  	    int i = 0;
-  
-    	// on a le tas pour y mettre tous les labels (pour ordonner le coutCourant)
-    	BinaryHeap<Label> tasLabel= new BinaryHeap<Label>();
-    	
-    	// une liste qui stocke les labels de tous les noeuds
-    	ArrayList<Label> listeLabel = new ArrayList<Label>();
-    	
     	// on met labels de tous les noeuds dans la liste
     	for (i=0; i<numNoeudGraphe ; i++){
     		if(this.graphe.getListeNoeuds().get(i).getId_noeud() == origine)
-    			listeLabel.add(new Label(origine, -1, 0, false));
+    		{
+    			Label labelOrigine = new Label(origine, -1, 0, false) ;
+    			listeLabel.add(labelOrigine);
+    			tasLabel.insert(labelOrigine);
+    		}
     		else
     			listeLabel.add(new Label(i));
     	}
     }
     
-    
     // algorithme de dijkstra
     
     public void algoDijkstra(){
+    	int i =0;
+    	// on a le label ayant le plus petit cout dans le tas 
+    	Label labelCourant = tasLabel.deleteMin();
+    	Noeud noeudCourant = this.graphe.getListeNoeuds().get(labelCourant.getId_sommetCourant());
     	
+    	for (Successeur succ : noeudCourant.getListeSuccesseur())
+    		labelCourant.setCoutCourant(labelCourant.getCoutCourant() + succ.getLongueurArrete());
     }
 
     public void run() {
 
 		System.out.println("Run PCC de " + zoneOrigine + ":" + origine + " vers " + zoneDestination + ":" + destination) ;
 		
-		//
+		// si la saisie est erronnée, inutile de lancer Dijkstra
 
-		if (verifierSaisirNoeudDepart())
+		if (!verifierSaisirNoeudDepart())
 			System.out.println("Le sommet d'origine " + origine + " n'existe pas dans cette carte.");
-		else if (verifierSaisirNoeudDest())
+		else if (!verifierSaisirNoeudDest())
 			System.out.println("Le sommet destination " + destination + " n'existe pas dans cette carte.");
 		else if (origine == destination)
 			System.out.println("Le plus court chemin est 0.");
+		// si la saisie est bonne, lancer Dijkstra
 		else {
+			initialisation();
 			algoDijkstra();
 		}
-	
     }
-
 }
