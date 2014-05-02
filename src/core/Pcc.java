0ce53645712571b4bec3ad6578cbd1563ec6c6f7
yuanbo07 <y_wang@etud.insa-toulouse.fr>
2014-05-02 19:28:21 +0200

    edited pcc

--- src/core/Pcc.java
+++ src/core/Pcc.java
@@ -88,62 +88,54 @@ public class Pcc extends Algo {
     
     public void algoDijkstra(){
     	
-    	System.out.println("la taille de tas avant initialisation: " + tasLabel.getCurrentSize());
-    	System.out.println("hashmap : " + mapCorrespondanceNoeudLabel.size());
-    	initialisationAlgoDijkstra();
-    	System.out.println("la taille de tas après initialisation: " + tasLabel.getCurrentSize());
-    	System.out.println("hashmap après : " + mapCorrespondanceNoeudLabel.size());
-    	System.out.println("hashmap test : " + mapCorrespondanceNoeudLabel.get(this.graphe.getListeNoeuds().get(origine).getId_noeud()));
-        System.out.println("la taille de tas dans la boucle : " + tasLabel.getCurrentSize());
-        	
-        	/* 
-        	 * phase déroulement algo Dijkstra
-        	 */
-
+    	// initialiser
+    	this.initialisationAlgoDijkstra();
+  
+    	// tourner l'algo jusqu'à noeud destination soit marqué
 		do{
-		
-    	// si le tas n'est pas vide
-    	if(!(tasLabel.isEmpty())) {
-        	// on choisie label ayant le plus petit cout dans le tas
-        	Label labelCourant = tasLabel.deleteMin();
-        	Noeud noeudCourant = this.graphe.getListeNoeuds().get(labelCourant.getId_sommetCourant());
-        	// on le marque
-        	labelCourant.setMarquage(true);
-	        	// pour tous ses successeurs (tous les arcs sortants)
-	        	for (Successeur succ : noeudCourant.getListeSuccesseur()){
-	        		// on choisie le noeud succ courant
-	        		Noeud noeudSuccCourant = succ.getNoeudDestination();
-	        		// on a son label
-	        		Label labelNoeudSuccCourant = mapCorrespondanceNoeudLabel.get(noeudSuccCourant.getId_noeud());
-	        		System.out.println("" + labelNoeudSuccCourant.getId_sommetCourant());
-	        		// si ce noeud successeur n'est pas encore marqué
-		    		if(!labelNoeudSuccCourant.isMarque()){
-		    			double cout = 0;
-		    			// on met à jour son cout courant (de sommet d'origine jusqu'ici)
-		    			cout = labelCourant.getCoutCourant() + succ.getLongueurArrete();
-		    			// si cette fois, le cout total obtenu est inférieur à son cout total avant
-		    			if (cout < labelNoeudSuccCourant.getCoutCourant()) {
-		    				// on remplace l'ancien cout avec ce nouveau cout total 
-		    				labelNoeudSuccCourant.setCoutCourant(cout);
-		    				// on change son père à ce nouveau noeud 
-		    				labelNoeudSuccCourant.setId_sommetPere(noeudCourant.getId_noeud());
-		    			}
-		    			
-		    			// si ce noeud n'est pas dans le tas
-		    			if(!tasLabel.isInHeap(labelNoeudSuccCourant)){
-		    				// on met ce label dans le tas
-		    				tasLabel.insert(labelNoeudSuccCourant);
-		    				// on dessine un segment entre noeudCourant et son succCourant
-							this.graphe.getDessin().setColor(Color.cyan);
-							this.graphe.getDessin().setWidth(5);
-							this.graphe.getDessin().drawLine(noeudCourant.getLongitude(), noeudCourant.getLatitude(), noeudSuccCourant.getLongitude(), noeudSuccCourant.getLatitude());
-		    			}
-		    			else
-				    		tasLabel.update(labelNoeudSuccCourant);
-		    		}
-	        	} 
-    		}
-    	}while(!mapCorrespondanceNoeudLabel.get(destination).isMarque());
+	    	// si le tas n'est pas vide
+	    	if(!(tasLabel.isEmpty())) {
+	        	// on choisie label ayant le plus petit cout dans le tas
+	        	Label labelCourant = tasLabel.deleteMin();
+	        	// on le marque
+	        	labelCourant.setMarquage(true);
+	        	
+	        	Noeud noeudCourant = this.graphe.getListeNoeuds().get(labelCourant.getId_sommetCourant());
+		        	// pour tous ses successeurs (tous les arcs sortants)
+		        	for (Successeur succ : noeudCourant.getListeSuccesseur()){
+		        		// on choisie le noeud succ courant
+		        		Noeud noeudSuccCourant = succ.getNoeudDestination();
+		        		// on a son label à partir de son id
+		        		Label labelNoeudSuccCourant = mapCorrespondanceNoeudLabel.get(noeudSuccCourant.getId_noeud());
+		        		System.out.println("" + labelNoeudSuccCourant.getId_sommetCourant());
+		        		// si ce noeud successeur n'est pas encore marqué
+			    		if(!labelNoeudSuccCourant.isMarque()){
+			    			double cout = 0;
+			    			// on met à jour son cout courant (de sommet d'origine jusqu'ici)
+			    			cout = labelCourant.getCoutCourant() + succ.getLongueurArrete();
+			    			// si cette fois, le cout total obtenu est inférieur à son cout total avant
+			    			if (cout < labelNoeudSuccCourant.getCoutCourant()) {
+			    				// on remplace l'ancien cout avec ce nouveau cout total 
+			    				labelNoeudSuccCourant.setCoutCourant(cout);
+			    				// on change son père à ce nouveau noeud 
+			    				labelNoeudSuccCourant.setId_sommetPere(noeudCourant.getId_noeud());
+			    				// on met à jour le tas
+					    		tasLabel.update(labelNoeudSuccCourant);
+			    			}
+			    			
+			    			// si ce noeud n'est pas dans le tas
+			    			if(!tasLabel.isInHeap(labelNoeudSuccCourant)){
+			    				// on met ce label dans le tas
+			    				tasLabel.insert(labelNoeudSuccCourant);
+			    				// on dessine un segment entre noeudCourant et son succCourant
+								this.graphe.getDessin().setColor(Color.cyan);
+								this.graphe.getDessin().setWidth(5);
+								this.graphe.getDessin().drawLine(noeudCourant.getLongitude(), noeudCourant.getLatitude(), noeudSuccCourant.getLongitude(), noeudSuccCourant.getLatitude());
+			    			}
+			    		}
+		        	} 
+	    	}
+    	} while(!mapCorrespondanceNoeudLabel.get(destination).isMarque());
     }
 
     public void run() {
