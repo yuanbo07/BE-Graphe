2560b01fe19452539a50c727da296bff5e65801a
yuanbo07 <y_wang@etud.insa-toulouse.fr>
2014-05-31 19:22:21 +0200

    *covoiturage 1ere partie finie, tout marche. à faire zpieton*

---
+++ src/core/SimpleTest.java
@@ -0,0 +1,40 @@
+package core;
+
+import java.io.IOException;
+import base.Readarg;
+
+/**
+ * La classe simpleTest est pour le but de calculer tous les chemins possibles entre sommets dans un graphe
+ * Eviter d'utiliser ce test pour la carte de grande taille, dans ce cas, utiliser "Sort test"
+ */
+
+public class SimpleTest extends Algo {
+	
+	public int choixAlgo;
+	public int isEnTemps;
+	
+	public SimpleTest(Graphe gr, Readarg readarg){
+		super(gr) ;
+		this.choixAlgo = readarg.lireInt ("Lancer le test en Dijkstra Standard(0) ou Dijkstra A Star(1) ?");
+		this.isEnTemps = readarg.lireInt ("Lancer le test en distance(0) ou temps(1) ?");
+		runSimpleTest() ;
+	}
+	
+	public void runSimpleTest(){
+		// pour tous les noeuds dans une carte, on lance l'algorithme Pcc
+		int size = Algo.graphe.getListeNoeuds().size() ;
+		System.out.println("size is : " + size);
+		for(int i=0; i < size ; i++) {
+			for(int j=0; j < size ; j++) {
+				// si les deux noeuds sont bien dans la carte
+				if(graphe.getListeNoeuds().get(i) != null && graphe.getListeNoeuds().get(j) != null){
+					Pcc test = new Pcc(graphe, i, j, choixAlgo, isEnTemps);
+					test.run();
+				}
+			}
+		}
+	}
+	
+	public void run() throws IOException {
+	}
+}
